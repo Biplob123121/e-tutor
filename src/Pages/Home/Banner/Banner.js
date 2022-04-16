a3da5cd3125feb2470ae807fb7aee87b73608e65
Biplob123121 <biplobkb123@gmail.com>
2022-04-17 05:15:24 +0600

    header section and banner section completed

---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import banner from '../../../banner.png';
+import './Banner.css';
+
+const Banner = () => {
+    return (
+        <section className='banner-container'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-sm-12 col-md-12 col-lg-6 d-flex align-items-center'>
+                        <div>
+                            <h1>Welcome to <span id='site-title'>E-Tutor</span></h1>
+                            <h4>A Reliable Person can show you a big Dream!!</h4>
+                            <p>I am Biplob Kumar, a tutor of online program. I take some courses in online. I have provided this services for 5 years. You can join with me and enjoy with me...</p>
+                            <button className='banner-btn'>JOIN NOW</button>
+                        </div>
+                    </div>
+                    <div className='col-sm-12 col-md-12 col-lg-6 d-flex align-items-center justify-content-center'>
+                        <img className='banner-image' src={banner} alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Banner;
