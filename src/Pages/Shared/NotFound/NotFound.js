c1fa97cc007cbba7d3b6a8f6f39ed530c3d55410
Biplob123121 <biplobkb123@gmail.com>
2022-04-18 17:50:24 +0600

    404 route completed

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import './NotFound.css';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>404</h1>
-            <h3>The page is not Found!!!</h3>
-        </div>
+        <section className='not-found-section d-flex align-items-center'>
+            <div className='container text-center'>
+                <h1 className='text-danger'>404</h1>
+                <h3 className='text-primary fs-1'>This page is not Found!!!</h3>
+            </div>
+        </section>
     );
 };
 
